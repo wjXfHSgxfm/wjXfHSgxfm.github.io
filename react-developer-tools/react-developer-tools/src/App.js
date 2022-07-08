e96e75b4ac7e9cfdf0368e53a40aa002745d5af7
Christoph H <wjXfHSgxfm@protonmail.com>
2022-07-08 21:06:04 +0200

    Added fcc quote generator

---
+++ react-developer-tools/react-developer-tools/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload. https://react-devtools-tutorial.vercel.app/element-selector-tool
+        </p>
+        {props.name}
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ react-recipe-app/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './App.css';
+
+
+const App = () =>{
+
+  const APP_ID = '8a1e92fe';
+  const APP_KEY = 'b93186729f33c030a677ebe023762c6d';
+
+  return (
+    <div className="App">
+
+    </div>
+  )
+}
+
+export default App;
